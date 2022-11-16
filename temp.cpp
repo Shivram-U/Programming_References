174aa17ded88ac1f01dc41d6aced9a21bf6cbc69
Shivram U <92662851+Shivram-U@users.noreply.github.com>
2022-11-16 15:30:16 +0530

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -3,6 +3,9 @@
 #include<stdlib.h>
 #define temp Arith_OP_OL
 #define temp1 Relat_OP_OL
+#define temp2 Log_OP_OL
+#define temp3 Ass_OP_OL
+#define temp4 Spec_OP_OL
 using namespace std;
 
 // Type Conversion Operator Overloading
@@ -31,13 +34,23 @@ Operator Overloading:
                                         No restriction in Process to be given.
                                 
                                 The above mentioned Features are restricted, to the Situation, in which the Operator is used.
+        
+        SYNTAX:
+                       returnType operator symbol (arguments) 
+                       {
+                            … .. …
+                       } 
 Reference:
     1. https://stackoverflow.com/questions/58010917/scope-operator-in-operator-overloading#:~:text=If%20you%20define%20the%20operator,)%20%7B%20%2F%2F%20...%20%7D
+    2.Types of Operators:
+        https://www.geeksforgeeks.org/operators-in-cpp/
+    2. Diferent types of Operaqtors Voerload:
+        https://www.datatrained.com/post/types-of-operator-overloading-in-c/?utm_source=rss&utm_medium=rss&utm_campaign=types-of-operator-overloading-in-c
 */
 
 
 
-class Arith_OP_OL                                                                              // Arithmetic Operators Overloading
+class Arith_OP_OL                                                                                           // Arithmetic Operators Overloading
 {
     public:
     int t[10];
@@ -60,13 +73,6 @@ class Arith_OP_OL                                                               
         this->t[0]++;
         this->t[1]++;
         this->t[2]++;
-    }
-    
-    void operator +=(int a)
-    {
-        cout<<"+= Operator Overloaded\n";
-        for(int i=0;i<3;i++)
-            this->t[i]+=a;
     }
     
     temp operator +(temp& t)                                                            // Operator Overloaded inside CLASS SCOPE.
@@ -107,7 +113,7 @@ temp temp::operator /(temp &t1)                                                 
 }
 
 
-class Relat_OP_OL
+class Relat_OP_OL                                                                                           // Relational Operator Overloading
 {
     public :
     int main;
@@ -165,32 +171,107 @@ class Relat_OP_OL
         return (t.main == main)?1:0;
     }
 };
-/*
-class temp1
+
+class Log_OP_OL                                                                                         // Logical Operator Overloading
+{
+    public:
+    int main;
+    temp2(int a)
+    {
+        main = a ;
+    }
+    
+    void display()
+    {
+        cout<<"Main : "<<main<<'\n';
+    }
+    void operator && (int b)
+    {
+        cout<<"&& Operator overloaded int class temp3"<<" passed Data: "<<main<<','<<b<<'\n';
+        if(this->main == b)
+            cout<<"Equal\n";
+        else
+            cout<<"Not Equal\n";
+    }
+    //int operator && (int b)                                                     // Causes Ambiguity Error
+    int operator && (temp2 t1)
+    {
+        if(this->main == t1.main)
+            return 1;
+        else
+            return 0;
+    }
+    
+    bool operator || (temp2 t)
+    {
+        if(this->main>0 ||  t.main>0)
+            return true;
+        else
+            return false;
+    }
+    
+    void operator ! ()
+    {
+        this->main = -main;
+    }
+};
+
+class Ass_OP_OL                                                                                     // Assignment Operator Overloading
+{
+    public:
+    int t[10];
+    temp3(int k)
+    {
+        for(int i=0;i<10;i++)
+        {
+            t[i] = i+k;
+        }
+    }
+    void display()
+    {
+        for(int i=0;i<10;i++)
+            cout<<t[i]<<' ';
+        cout<<'\n';
+    }
+    
+    void operator =(temp3 t1)
+    {
+        for(int i=0;i<10;i++)
+            this->t[i] = t1.t[i];
+    }
+    
+    void operator +=(int x)
+    {
+        for(int i=0;i<10;i++)
+            this->t[i]+=x;
+    }
+};
+
+class Spec_OP_OL                                                                                    // Special Operators Overloading
 {
     public:
     int t[4];
-    temp1(int a,int b,int c)
+    temp4(int a,int b,int c)
     {
         this->t[0] = a;
         this->t[1] = b;
         this->t[2] = c;
-    }
-    temp1(){}                                                                                // Default Constructor, if not Declared, nomral Object Declaration is not allowed.
+    }                                                                               // Default Constructor, if not Declared, a Default constructor will be provided by the Compiler itself,
+    temp4(){}                                                                       // Default Constructor is requred to use the 'new y=temp4()'.
     void print()
     {
         cout<<"t[0] : "<<this->t[0]<<" t[1] : "<<this->t[1]<<" t[2] : "<<this->t[2]<<'\n';
     }
     
-    
+    /*
     void* operator new (size_t size)
     {
         return (void*)::new long int(10);                                           // Memory Management, NEW SCENARIO 
-    }
+    }*/
     
     void* operator new(size_t size)
     {
-        cout<<"New Operator Overloaded with respect to Class 'temp1'\n";
+        cout<<"New Operator Overloaded with respect to Class 'temp4'\n";
         return ::operator new(size);
         //  Here the Point is, we are returning only the ADDRESS of the MEMORY allocated, no type is specified, inside the function, Since the return type is 'VOID*'.
         //  return (void*)::operator new(size);                                           // Valid
@@ -202,13 +283,12 @@ class temp1
     
     void operator delete(void* p)
     {
-        cout<<"Delete Operator Overloaded with respect to Class 'temp1'\n";
+        cout<<"Delete Operator Overloaded with respect to Class 'temp4'\n";
         free(p);
         //delete p;
     }
     
 };
-*/
 
 
 // Global Operator Overloading:
@@ -235,7 +315,7 @@ void* operator new(size_t size)
 
 int main()
 {
-    // Operator OVerloading
+    // Operator Overloading
         // GLobal Scope:
             +3;
     //Local(Class) Scope        
@@ -260,12 +340,51 @@ int main()
         t5.print();
         //printf("[%d]",t3);
         */
+        
         //Relational Operator Overloading:
+        /*
         temp1 t11(10,20,30),t12(100,200,300), t13(10,20,30);
         printf("Data Member Main in t11 is %s\n",(t11 > t12)?"Less":"Greater");
         printf("Data Member Main int t11 is %s\n",(t11 < t12)?"Greater":"Less");
         printf("Data Member Main is %s\n",(t11 == t13)?"Equal":"Not Equal");
         t11<=10;
+        */
+        
+        //Logical Operator Overloading:
+        /*
+        temp2 t21(100),t22(1000),t23(10000);
+        t21 && 1023;                                                    // && operator Overloaded
+        cout<<((t21 && t22)?"Equal":"Not Equal")<<'\n';
+        cout<<((t21 || t22)?"true":"false")<<'\n';
+        t21.display();
+        !t21;
+        t21.display();
+        */
+        
+        //Assignment Operator Overloading
+        /*
+        temp3 t31(0),t32(5);
+        t31.display();
+        t32.display();
+        t31 = t32;
+        t31.display();
+        t31+=123;
+        t31.display();
+        */
+        
+        // Special Operators Overloading: 
+        
+        // New and Delete Operator Overload:
+        /*
+            temp4 t41(1,2,3),t42(4,5,6),*tp1,*tp2;
+            t41.print();
+            t42.print();
+            tp1 = new temp4;
+            tp2 = new temp4();
+            delete(tp1);
+            delete(tp2);
+        */
+        
     // Overloading Methodologies:
     /*
     temp1 t11(9,8,7),t12(6,5,4);
